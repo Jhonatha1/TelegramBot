192d535e42be781e595c7071cedd7e02e7703ea7
Yanky Jhonatha <88850852+Jhonatha1@users.noreply.github.com>
2022-06-13 22:24:59 -0300

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(CHAVE_API)
 @bot.message_handler(commands=["op1"])
 def op1(mensagem):
     print(mensagem)
-    bot.send_message(mensagem.chat.id, "Tamo junto! O Yanky mandou um abraço de volta :).")
+    bot.send_message(mensagem.chat.id, "Tamo junto! O Yanky mandou um abraço de volta :)")
 
 
 @bot.message_handler(commands=["op2"])
@@ -23,6 +23,7 @@ def op2(mensagem):
 @bot.message_handler(commands=["op3"])
 def op3(mensagem):
     bot.send_location(mensagem.chat.id, latitude= -20.186861, longitude= -44.772278)
+    bot.send_message(mensagem.chat.id, "O dono do bot, conhecido como Yanky, mora em Carmo do Cajuru - Minas Gerais.")
 
 
 
@@ -33,7 +34,7 @@ def verificar(mensagem):
 @bot.message_handler(func=verificar)
 def responder(mensagem):
     texto = """
-    Escolha uma opção para continuar:
+    Olá, sou o bot do Yanky, para continuar, escolha, clicando, em uma das 3 opções:
 /op1 Mandar um abraço pro Yanky.
 /op2 Saber mais sobre o bot.  
 /op3 Saber onde o dono do bot nasceu. 
